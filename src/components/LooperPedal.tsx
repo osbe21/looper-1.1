d92dc56269d35afb26a5fd19e61a4e97ac0ad125
Oscar Berge <osbe031251@gmail.com>
2025-12-23 12:25:23 +0100

    Fjernet setMicrophoneSettings importen fra LooperPedal

--- src/components/LooperPedal.tsx
+++ src/components/LooperPedal.tsx
@@ -28,7 +28,6 @@ export default function LooperPedal() {
         resumeAudioContext,
         footswitch,
         setGain: setLooperGain,
-        setMicrophoneSettings,
     } = useLooperEngine();
 
     const [gain, setGain] = useState(1);
