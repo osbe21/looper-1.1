b037f9810329c7c3b084379d3d977c364bfb98a6
Oscar Berge <osbe031251@gmail.com>
2026-01-03 05:45:03 +0100

    fix: fikset spacing inni how to use dialog

--- src/components/HowToUseDialog.tsx
+++ src/components/HowToUseDialog.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from "./ui/scroll-area";
 import { H1, H2, P } from "./Typography";
 import { Button } from "./ui/button";
 import {
@@ -19,59 +20,65 @@ export default function InformationDialog() {
                 </Button>
             </DialogTrigger>
 
-            <DialogHeader className="sr-only">
-                <DialogTitle>How to use looper/1.1</DialogTitle>
-            </DialogHeader>
-
             <DialogContent>
-                <H1>
-                    Welcome to{" "}
-                    <span className="font-mono italic">looper/1.1</span>
-                </H1>
+                <DialogHeader className="sr-only">
+                    <DialogTitle>How to use looper/1.1</DialogTitle>
+                </DialogHeader>
 
-                <P>
-                    Record, layer, and build epic guitar loops right in your
-                    browser!
-                </P>
+                <ScrollArea className="max-h-[70vh]">
+                    <div className="space-y-4">
+                        <H1>
+                            Welcome to{" "}
+                            <span className="font-mono italic">looper/1.1</span>
+                        </H1>
 
-                <H2>Quick Start</H2>
+                        <P>
+                            Record, layer, and build epic guitar loops right in
+                            your browser!
+                        </P>
 
-                <ol className="ml-4 list-outside list-decimal">
-                    <P>
-                        <li>
-                            Hit the footswitch or spacebar to start recording
-                            your riff.
-                        </li>
-                        <li>
-                            Play your loop once, then press again to stop and
-                            start playback.
-                        </li>
-                        <li>
-                            Layer overdubs by pressing the footswitch during
-                            playback.
-                        </li>
-                        <li>Use Undo to remove the last overdub.</li>
-                    </P>
-                </ol>
+                        <H2>Quick Start</H2>
 
-                <H2>Pro Tips</H2>
+                        <ol className="ml-4 list-outside list-decimal">
+                            <P>
+                                <li>
+                                    Hit the footswitch or spacebar to start
+                                    recording your riff.
+                                </li>
+                                <li>
+                                    Play your loop once, then press again to
+                                    stop and start playback.
+                                </li>
+                                <li>
+                                    Layer overdubs by pressing the footswitch
+                                    during playback.
+                                </li>
+                                <li>Use Undo to remove the last overdub.</li>
+                            </P>
+                        </ol>
 
-                <ul className="ml-4 list-outside list-disc">
-                    <P>
-                        <li>Grant mic access for recording.</li>
-                        <li>
-                            Select the correct input (guitar interface or mic).
-                        </li>
-                        <li>
-                            Compatible with Fender Mustang Micro, Boss Katana
-                            Go, etc.
-                        </li>
-                        <li>
-                            If you're experiencing too much latency, you can
-                            manually increase compensation in the settings.
-                        </li>
-                    </P>
-                </ul>
+                        <H2>Pro Tips</H2>
+
+                        <ul className="ml-4 list-outside list-disc">
+                            <P>
+                                <li>Grant mic access for recording.</li>
+                                <li>
+                                    Select the correct input (guitar interface
+                                    or mic).
+                                </li>
+                                <li>
+                                    Compatible with Fender Mustang Micro, Boss
+                                    Katana Go, etc.
+                                </li>
+                                <li>
+                                    If you're experiencing too much latency, you
+                                    can manually increase compensation in the
+                                    settings.
+                                </li>
+                            </P>
+                        </ul>
+                    </div>
+                </ScrollArea>
 
                 <DialogFooter className="sm:justify-start">
                     <DialogClose asChild>
